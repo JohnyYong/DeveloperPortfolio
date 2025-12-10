d2132797bf749e19948fcc6a4f522d94d8aff1d3
JohnyYong <Flamejohny@gmail.com>
2025-12-10 17:20:39 +0800

    Tools

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
+
+// --- Tool Icons ---
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
-import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
+import PhotoShop from "../../Assets/TechIcons/PhotoShop.svg";
+import Maya from "../../Assets/TechIcons/Maya.svg";
+import VS from "../../Assets/TechIcons/VS2022.svg";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+
+      {/* Google Chrome */}
       <Col xs={4} md={2} className="tech-icons">
-        <img src={macOs} alt="macOs" className="tech-icon-images" />
-        <div className="tech-icons-text">Mac Os</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={chrome} alt="Chrome" className="tech-icon-images" />
+        <img src={chrome} alt="Google Chrome" className="tech-icon-images" />
         <div className="tech-icons-text">Google Chrome</div>
       </Col>
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={vsCode} alt="vsCode" className="tech-icon-images" />
-        <div className="tech-icons-text">Vs Code</div>
+
+      {/* VS Code */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={vsCode} alt="VS Code" className="tech-icon-images" />
+        <div className="tech-icons-text">VS Code</div>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={intelliJ} alt="go" className="tech-icon-images" />
-        <div className="tech-icons-text">IntelliJ</div>
+      {/* Visual Studio 2022 */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={VS} alt="Visual Studio 2022" className="tech-icon-images" />
+        <div className="tech-icons-text">Visual Studio 2022</div>
       </Col>
+
+      {/* Adobe Photoshop */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={PhotoShop} alt="Adobe Photoshop" className="tech-icon-images" />
+        <div className="tech-icons-text">Photoshop</div>
+      </Col>
+
+      {/* Autodesk Maya */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Maya} alt="Autodesk Maya" className="tech-icon-images" />
+        <div className="tech-icons-text">Autodesk Maya</div>
+      </Col>
+
     </Row>
   );
 }
