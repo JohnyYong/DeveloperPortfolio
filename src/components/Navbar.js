956eaf94aec11e407511e17e66d0456863d8e410
JohnyYong <Flamejohny@gmail.com>
2025-12-10 14:08:17 +0800

    Switch to HashRouter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
             </Nav.Item>
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://johnyyong.github.io/DeveloperPortfolio/"
                 target="_blank"
                 className="fork-btn-inner"
               >
