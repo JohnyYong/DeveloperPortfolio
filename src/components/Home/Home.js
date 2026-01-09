7c17747cba1918f3ada558933d3ada06e94a9144
JohnyYong <Flamejohny@gmail.com>
2026-01-09 20:51:49 +0800

    Notable projects at front

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,70 @@ import homeLogo from "../../Assets/home-main.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import Snowfall from "../Snowfall"
-import Techstack from "../About/Techstack";
-import Github from "../About/Github";
-import {
-  AiFillGithub,
-} from "react-icons/ai";
+import Snowfall from "../Snowfall";
+
+import ProjectCoverflow from "../Projects/ProjectCoverflow";
+import grabity from "../../Assets/Projects/GrabityTitle.gif";
+import AzureVania from "../../Assets/Projects/AzureVania.gif";
+import Obelisk from "../../Assets/Projects/Obelisk.png";
+
+import C from "../../Assets/TechIcons/C++.svg";
+import FMod from "../../Assets/TechIcons/fmod.svg";
+import OpenGL from "../../Assets/TechIcons/OpenGL.svg";
+import CSharp from "../../Assets/TechIcons/CSharp.svg";
+import Unity from "../../Assets/TechIcons/Unity.svg";
+import Forge from "../../Assets/TechIcons/ForgeNetworking.png";
+import Oculus from "../../Assets/TechIcons/Oculus.svg";
+import PlayStation from "../../Assets/TechIcons/PlayStation.svg";
+import Lua from "../../Assets/TechIcons/Lua.svg";
+
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
+  const featuredProjects = [
+    {
+      imgPath: grabity,
+      title: "Grabity",
+      isFeatured: true,
+      description: "Grabity is a physics-driven top-down survival game built on our team's custom game engine. I served as Product Manager and led core gameplay development.",
+      ghLink: "https://github.com/JohnyYong/Grabity",
+      docsLink: "https://johnyyong.github.io/GrabityDocumentation/",
+      skills: [
+        { src: C, label: "C++" },
+        { src: FMod, label: "FMod" },
+        { src: OpenGL, label: "OpenGL" },
+        { src: Lua, label: "Lua" },
+      ],
+    },
+    {
+      imgPath: AzureVania,
+      title: "AzureVania",
+      isFeatured: true,
+      description: "A 2D mini Metroidvania Unity game project. My role included handling Multiplayer Networking, Level Design and Enemy AIs.",
+      ghLink: "https://github.com/JohnyYong/AzureVania",
+      skills: [
+        { src: CSharp, label: "C#" },
+        { src: Unity, label: "Unity" },
+        { src: Forge, label: "Forge Networking" },
+      ],
+    },
+    {
+      imgPath: Obelisk,
+      title: "Obelisk",
+      isFeatured: true,
+      description: "Obelisk is a large-scale interdisciplinary VR project. I acted as Team Leader and Programming Lead, managing technical direction and Oculus VR implementation.",
+      driveLink: "https://drive.google.com/drive/u/0/folders/14Y1LOmjX-xWqlVCej7PvYdkGI0lrqGw8",
+      demoLink: "https://www.youtube.com/playlist?list=PL9g68ppKwXMmE_Nh0aGJ9Th2hFns0YCvn",
+      skills: [
+        { src: CSharp, label: "C#" },
+        { src: Unity, label: "Unity" },
+        { src: Oculus, label: "Oculus VR" },
+        { src: PlayStation, label: "PS4 Devkit" },
+      ],
+    },
+  ];
+
   return (
     <section>
       <Container fluid className="home-section" id="home">
@@ -23,55 +78,43 @@ function Home() {
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Welcome to my page!{" "}
               </h1>
-
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> JOHNY YONG JUN SIANG</strong>
+                I'M <strong className="main-name"> JOHNY YONG JUN SIANG</strong>
               </h1>
-
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
             </Col>
-
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+              <img src={homeLogo} alt="home pic" className="img-fluid" style={{ maxHeight: "450px" }} />
             </Col>
           </Row>
         </Container>
       </Container>
+
+      {/* 3. Featured Projects Carousel */}
+      <Container style={{ marginTop: "50px" }}>
+        <h1 className="project-heading" style={{ textAlign: "center" }}>
+          Notable <strong className="cyan"> Projects</strong>
+        </h1>
+        <ProjectCoverflow projects={featuredProjects} />
+      </Container>
+
       <Home2 />
 
       <Container>
         <Row style={{ paddingTop: "50px", paddingBottom: "80px" }}>
           <Col md={12} className="home-about-social">
             <h1>Find Me On</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
+            <p>Feel free to <span className="purple">connect </span>with me</p>
             <ul className="home-about-social-links">
               <li className="social-icons">
-                <a
-                  href="https://github.com/JohnyYong"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
+                <a href="https://github.com/JohnyYong" target="_blank" rel="noreferrer" className="icon-colour home-social-icons">
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/johnyyong/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
+                <a href="https://www.linkedin.com/in/johnyyong/" target="_blank" rel="noreferrer" className="icon-colour home-social-icons">
                   <FaLinkedinIn />
                 </a>
               </li>
--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,7 +4,6 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import { SiFigma, SiGoogledrive } from "react-icons/si";
-// 1. Added a documentation icon
 import { HiOutlineDocumentText } from "react-icons/hi"; 
 import { Row, Col } from "react-bootstrap";
 
@@ -13,7 +12,6 @@ function ProjectCards(props) {
   const hasSoftSkills = Array.isArray(props.softSkills) && props.softSkills.length > 0;
   const showAnySkills = hasHardSkills || hasSoftSkills;
 
-  // Soft-skill collapsing (keeps card height under control)
   const softSkillLimit = typeof props.softSkillLimit === "number" ? props.softSkillLimit : 6;
   const [showAllSoftSkills, setShowAllSoftSkills] = useState(false);
 
@@ -27,8 +25,12 @@ function ProjectCards(props) {
     ? Math.max(0, props.softSkills.length - softSkillsToShow.length)
     : 0;
 
-  // --- Inline styles ---
   const styles = {
+    cardExtra: props.isFeatured ? {
+      border: "2px solid #ffeb3b", 
+      boxShadow: "0px 0px 15px rgba(255, 235, 59, 0.3)",
+      transform: "scale(1.02)"
+    } : {},
     divider: {
       borderTop: "1px solid rgba(10, 136, 115, 0.94)",
       margin: "16px 0"
@@ -51,8 +53,6 @@ function ProjectCards(props) {
       opacity: 0.9,
       textAlign: "center"
     },
-
-    // Soft skills layout
     softSkillWrap: {
       display: "flex",
       flexWrap: "wrap",
@@ -82,32 +82,24 @@ function ProjectCards(props) {
   };
 
   return (
-    <Card className="project-card-view">
+    <Card className="project-card-view" style={styles.cardExtra}>
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
 
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
 
-        {/* Description */}
         <Card.Text style={{ textAlign: "justify", whiteSpace: "pre-line" }}>
           {props.description}
         </Card.Text>
 
-        {/* Divider (only if we have any skills to show) */}
         {showAnySkills && <div style={styles.divider} />}
 
-        {/* Hard Skills Section (icons) */}
         {hasHardSkills && (
           <>
             <div style={styles.sectionTitle}>Tech / Tools</div>
             <Row style={{ justifyContent: "center", marginBottom: "12px" }}>
               {props.skills.map((skill) => (
-                <Col
-                  key={skill.label}
-                  xs={4}
-                  md={3}
-                  style={{ textAlign: "center" }}
-                >
+                <Col key={skill.label} xs={4} md={3} style={{ textAlign: "center" }}>
                   <img src={skill.src} alt={skill.label} style={styles.skillIcon} />
                   <div style={styles.skillLabel}>{skill.label}</div>
                 </Col>
@@ -116,36 +108,21 @@ function ProjectCards(props) {
           </>
         )}
 
-        {/* Soft Skills Section (compact pills + +more toggle) */}
         {hasSoftSkills && (
           <>
             {hasHardSkills && <div style={{ ...styles.divider, margin: "12px 0" }} />}
-
             <div style={styles.sectionTitle}>Soft Skills</div>
-
             <div style={styles.softSkillWrap}>
               {softSkillsToShow.map((s) => (
-                <span key={s} style={styles.softSkillPill}>
-                  {s}
-                </span>
+                <span key={s} style={styles.softSkillPill}>{s}</span>
               ))}
-
               {!showAllSoftSkills && remainingSoftSkills > 0 && (
-                <button
-                  type="button"
-                  style={styles.softSkillMoreBtn}
-                  onClick={() => setShowAllSoftSkills(true)}
-                >
+                <button type="button" style={styles.softSkillMoreBtn} onClick={() => setShowAllSoftSkills(true)}>
                   +{remainingSoftSkills} more
                 </button>
               )}
-
               {showAllSoftSkills && props.softSkills.length > softSkillLimit && (
-                <button
-                  type="button"
-                  style={styles.softSkillMoreBtn}
-                  onClick={() => setShowAllSoftSkills(false)}
-                >
+                <button type="button" style={styles.softSkillMoreBtn} onClick={() => setShowAllSoftSkills(false)}>
                   Show less
                 </button>
               )}
@@ -153,58 +130,32 @@ function ProjectCards(props) {
           </>
         )}
 
-        {/* GitHub Button */}
         {props.ghLink && (
           <Button variant="primary" href={props.ghLink} target="_blank">
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
+            <BsGithub /> &nbsp; {props.isBlog ? "Blog" : "GitHub"}
           </Button>
         )}
 
-        {/* 2. Documentation Button */}
         {props.docsLink && (
-          <Button
-            variant="primary"
-            href={props.docsLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
+          <Button variant="primary" href={props.docsLink} target="_blank" style={{ marginLeft: "10px" }}>
             <HiOutlineDocumentText /> &nbsp; Docs
           </Button>
         )}
 
-        {/* Google Drive Button */}
         {props.driveLink && (
-          <Button
-            variant="primary"
-            href={props.driveLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
+          <Button variant="primary" href={props.driveLink} target="_blank" style={{ marginLeft: "10px" }}>
             <SiGoogledrive /> &nbsp; Drive
           </Button>
         )}
 
-        {/* Demo Button */}
         {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
+          <Button variant="primary" href={props.demoLink} target="_blank" style={{ marginLeft: "10px" }}>
             <CgWebsite /> &nbsp; Demo
           </Button>
         )}
 
-        {/* Figma Button */}
         {props.figmaLink && (
-          <Button
-            variant="primary"
-            href={props.figmaLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
+          <Button variant="primary" href={props.figmaLink} target="_blank" style={{ marginLeft: "10px" }}>
             <SiFigma /> &nbsp; Figma
           </Button>
         )}
