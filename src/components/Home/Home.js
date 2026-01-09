dc250bf24b761f9b088b8c110aab5924f1ff7f66
JohnyYong <Flamejohny@gmail.com>
2026-01-09 21:03:43 +0800

    Lengthen the projects des

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.png";
-import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 import Snowfall from "../Snowfall";
@@ -14,12 +13,15 @@ import Obelisk from "../../Assets/Projects/Obelisk.png";
 import C from "../../Assets/TechIcons/C++.svg";
 import FMod from "../../Assets/TechIcons/fmod.svg";
 import OpenGL from "../../Assets/TechIcons/OpenGL.svg";
+import VS from "../../Assets/TechIcons/VS2022.svg";
+import vsCode from "../../Assets/TechIcons/vscode.svg";
+import PhotoShop from "../../Assets/TechIcons/PhotoShop.svg";
+import Lua from "../../Assets/TechIcons/Lua.svg";
 import CSharp from "../../Assets/TechIcons/CSharp.svg";
 import Unity from "../../Assets/TechIcons/Unity.svg";
 import Forge from "../../Assets/TechIcons/ForgeNetworking.png";
 import Oculus from "../../Assets/TechIcons/Oculus.svg";
 import PlayStation from "../../Assets/TechIcons/PlayStation.svg";
-import Lua from "../../Assets/TechIcons/Lua.svg";
 
 import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -28,42 +30,86 @@ function Home() {
   const featuredProjects = [
     {
       imgPath: grabity,
-      title: "Grabity",
+      isBlog: false,
       isFeatured: true,
-      description: "Grabity is a physics-driven top-down survival game built on our team's custom game engine. I served as Product Manager and led core gameplay development.",
+      title: "Grabity",
+      description:
+        "Grabity is a physics-driven top-down survival game built on our team's custom game engine. This project was my second experience working in a larger team of 9 members, where I served as the Product Manager.\n\n" +
+        "I led the core gameplay development and implemented the Audio System, Tag/Layer Manager, as well as the Game Object Factory along with it's Entity Component System architecture. The user interface of the engine is done by ImGui",
       ghLink: "https://github.com/JohnyYong/Grabity",
       docsLink: "https://johnyyong.github.io/GrabityDocumentation/",
       skills: [
         { src: C, label: "C++" },
         { src: FMod, label: "FMod" },
         { src: OpenGL, label: "OpenGL" },
+        { src: VS, label: "VS2022" },
+        { src: vsCode, label: "VSCode" },
+        { src: PhotoShop, label: "Photoshop" },
         { src: Lua, label: "Lua" },
       ],
+      softSkills: [
+        "Team Leadership",
+        "Cross-discipline Communication",
+        "Scope Management",
+        "Rapid Iteration",
+        "Problem Solving",
+        "Task Prioritization",
+      ],
+      softSkillLimit: 5,
     },
     {
       imgPath: AzureVania,
-      title: "AzureVania",
+      isBlog: false,
       isFeatured: true,
-      description: "A 2D mini Metroidvania Unity game project. My role included handling Multiplayer Networking, Level Design and Enemy AIs.",
+      title: "AzureVania",
+      description:
+        "A 2D mini Metroidvania Unity game project conducted a group of 4 including myself in 2020. Experimented in the usage of multiplayer networking through the Forge Networking package. My role in the project include handling Multiplayer Networking, Level Design and Enemy AIs.",
       ghLink: "https://github.com/JohnyYong/AzureVania",
       skills: [
         { src: CSharp, label: "C#" },
         { src: Unity, label: "Unity" },
+        { src: PhotoShop, label: "Photoshop" },
         { src: Forge, label: "Forge Networking" },
+        { src: VS, label: "VS2022" },
+        { src: vsCode, label: "VSCode" },
       ],
+      softSkills: [
+        "Multiplayer Systems Thinking",
+        "Team Collaboration",
+        "Technical Communication",
+        "Problem Solving",
+        "Iterative Development",
+        "Responsibility Ownership",
+      ],
+      softSkillLimit: 5,
     },
     {
       imgPath: Obelisk,
-      title: "Obelisk",
+      isBlog: false,
       isFeatured: true,
-      description: "Obelisk is a large-scale interdisciplinary VR project. I acted as Team Leader and Programming Lead, managing technical direction and Oculus VR implementation.",
+      title: "Obelisk",
+      description:
+        "Obelisk is my Final Year Project and first large-scale interdisciplinary collaboration, developed over 3 months for virtual reality. The project was originally built for PlayStation 4 VR, but midway through development, the team pivoted to Oculus Quest due to COVID-19 restrictions.\n\n" +
+        "I took on the role of Team Leader and Programming Lead, overseeing both technical direction and cross-discipline coordination with artists. My core contributions included Oculus VR implementation, grid-based interaction systems, VR input handling, and custom shader development.\n\n" +
+        "This project was a key learning experience in adapting technical pipelines under real-world constraints, managing scope changes, and leading a mixed-discipline team to deliver a complete VR game within a tight production timeline.",
       driveLink: "https://drive.google.com/drive/u/0/folders/14Y1LOmjX-xWqlVCej7PvYdkGI0lrqGw8",
       demoLink: "https://www.youtube.com/playlist?list=PL9g68ppKwXMmE_Nh0aGJ9Th2hFns0YCvn",
       skills: [
         { src: CSharp, label: "C#" },
         { src: Unity, label: "Unity" },
+        { src: PhotoShop, label: "Photoshop" },
         { src: Oculus, label: "Oculus VR" },
-        { src: PlayStation, label: "PS4 Devkit" },
+        { src: PlayStation, label: "PlayStation4 Devkit" },
+        { src: VS, label: "VS2022" },
+        { src: vsCode, label: "VSCode" },
+      ],
+      softSkills: [
+        "Technical Leadership",
+        "Cross-Disciplinary Collaboration",
+        "Scope & Risk Management",
+        "Adaptability",
+        "Production Planning",
+        "Decision-Making",
       ],
     },
   ];
@@ -92,10 +138,10 @@ function Home() {
         </Container>
       </Container>
 
-      {/* 3. Featured Projects Carousel */}
+      {/* Featured Projects Section */}
       <Container style={{ marginTop: "50px" }}>
         <h1 className="project-heading" style={{ textAlign: "center" }}>
-          Notable <strong className="cyan"> Projects</strong>
+          Notable <strong className="cyan">Game Works</strong>
         </h1>
         <ProjectCoverflow projects={featuredProjects} />
       </Container>
